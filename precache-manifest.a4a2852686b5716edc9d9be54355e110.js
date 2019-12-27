798caa542ca1aa5eb7c7a2786dc111e15bd5de3b
code-plus-coffee <m416a@protonmail.com>
2019-12-27 19:05:11 +0530

    Updates

---
+++ precache-manifest.a4a2852686b5716edc9d9be54355e110.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a0a6745431cad0b0a0b374f734743a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "7278f8816a6b73d39730",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "2cc8f3a235ab9e66f4dd",
+    "url": "/robofriends/static/css/main.b2fa7c78.chunk.css"
+  },
+  {
+    "revision": "7278f8816a6b73d39730",
+    "url": "/robofriends/static/js/2.833464be.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.833464be.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2cc8f3a235ab9e66f4dd",
+    "url": "/robofriends/static/js/main.386e42b8.chunk.js"
+  },
+  {
+    "revision": "04454491b22da9ad3e7d",
+    "url": "/robofriends/static/js/runtime-main.98228a54.js"
+  }
+]);
