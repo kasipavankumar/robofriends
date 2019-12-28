7961f99ab5ad9c7ac4ed4f1314d177bbd7a4ec67
code-plus-coffee <m416a@protonmail.com>
2019-12-28 12:28:27 +0530

    Fixed few issues.

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import './Card.css';
 
 const Card = ({ name, email, id }) => {
     return(
-        <div className="tc bg-light-red dib br3 pa3 ma2 grow bw2 shadow-5">
+        <div className="tc bg-light-red dib br3 pa3 ma2 grow bw2 shadow-5 card">
             <img alt='robots' src={`https://robohash.org/${id}?set=set3`}/>
             <div>
                 <h2 id='robot-name'>{ name }</h2>
